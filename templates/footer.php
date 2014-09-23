27b4c38d6ab64a5cd6fb82ad990161cb4dd02afb
thamshid cc <thamshidcc@gmail.com>
2014-09-23 23:20:22 +0530

    footer added

---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+	<script type="text/javascript" src="<?php echo DOMAIN; ?>include/js/site.js"></script><footer id="footer">
+		<div class="container">
+			
+			<div style="padding: 15 600;float: left;">
+				(c) MtH 2014
+			</div>
+			<div style="padding: 0 600;float: left;">
+				Powered by MtHcC
+			
+	</footer>
+	</body>
+</html>
