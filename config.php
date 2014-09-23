b9bced6b853012ab7dc8c057db8526696156b323
thamshid cc <thamshidcc@gmail.com>
2014-09-23 23:16:40 +0530

    config.php added

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+ 
+
+$config = array();
+
+///////////////////// Application  ///////////////////////////
+
+define('FROOT', 'c:/server/www/mth/');	// File root of the project
+
+define("DOMAIN", "http://localhost/mth/"); /// Domain root
+
+////////////////////   Database   ////////////////////////////
+
+$config['db_host'] = '127.0.0.1';
+$config['db_name'] = 'mthly';
+$config['db_user'] = 'root';
+$config['db_pass'] = '';
+
+?>
