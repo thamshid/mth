990ffa2b7098609f07a02ba1d24b0d29cb1a41e9
thamshid cc <thamshidcc@gmail.com>
2014-09-23 23:20:51 +0530

    header added

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>MtH | Ultimate url shortner</title>
+
+		<link rel="stylesheet" href="<?php echo DOMAIN; ?>contents/css/w2ui-1.3.1.min.css" type="text/css" />
+		<link rel="stylesheet" href="<?php echo DOMAIN; ?>contents/css/style.css" type="text/css" />
+		<script src="<?php echo DOMAIN; ?>include/js/jquery-1.8.3.min.js" type="text/javascript"></script>
+		<script src="<?php echo DOMAIN; ?>include/js/w2ui-1.3.1.js" type="text/javascript"></script>
+		<script src="<?php echo DOMAIN; ?>include/js/jquery.form-validator.min.js" type="text/javascript"></script>
+		<script src="<?php echo DOMAIN; ?>include/js/site.js" type="text/javascript"></script>
+		<link rel="favicon" href="<?php echo DOMAIN;?>contents/images/favicon.png" type="image/png" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css' />
+		<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+		<script type="text/javascript"> 
+    var adfly_id = 2927722; 
+</script> 
+<script src="https://cdn.adf.ly/js/display.js"></script> 
+               
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="content">
+				<div id="header">
+					<div class="logo"><a href="<?php //echo DOMAIN; ?>"><img src="<?php //echo DOMAIN; ?>contents/images/logo.png" /></a>
+					</div>
+					
