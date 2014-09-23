2d5b6d35b028fa08d866602d2c3724420cfc9223
thamshid cc <thamshidcc@gmail.com>
2014-09-23 23:28:19 +0530

    learn mth

---
+++ templates/learn.php
@@ -0,0 +1,14 @@
+<section id="highlights">
+	<div class="lern_text">
+	MtH is an url shoter.URL shortening is a technique on the World Wide Web in which a Uniform Resource Locator (URL) may be made substantially shorter in length and still direct to the required page. This is achieved by using an HTTP Redirect on a domain name that is short, which links to the web page that has a long URL
+	</div>
+	<div class="linkto">
+	<a href=""style="color: whiteSmoke;">*Blog</a>
+	<a href=""style="color: whiteSmoke;">*Facebook</a>
+	<a href=""style="color: whiteSmoke;">*Contact</a>
+	</div>	
+	
+	
+	
+				
+</section>
